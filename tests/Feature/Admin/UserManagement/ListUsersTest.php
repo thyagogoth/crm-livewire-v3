71d992ac0b6c54219fc0c0c763288ffee2bfb7fb
Thiago F. da Rosa <thyagogoth@gmail.com>
2023-10-26 13:44:43 -0300

    crm-12: should be able to filter by permission key

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Enums\Can;
 use App\Livewire\Admin;
 
-use App\Models\User;
+use App\Models\{Permission, User};
 use Livewire\Livewire;
 
 use function Pest\Laravel\{actingAs, get};
@@ -75,6 +76,29 @@ it('should be able to filter by name and email', function () {
             expect($users)
                 ->toHaveCount(1)
                 ->first()->name->toBe('Mario');
+
+            return true;
+        });
+});
+
+it('should be able to filter by permission key', function () {
+    $admin      = User::factory()->admin()->create(['name' => 'Joe Doe', 'email' => 'admin@gmail.com']);
+    $normalUser = User::factory()->create(['name' => 'Mario', 'email' => 'little_guy@gmail.com']);
+    $permission = Permission::where(['key' => Can::BE_AN_ADMIN->value])->first();
+
+    actingAs($admin);
+    Livewire::test(Admin\Users\Index::class)
+        ->assertSet('users', function ($users) {
+            expect($users)
+                ->toHaveCount(2);
+
+            return true;
+        })
+        ->set('search_permissions', [$permission->id])
+        ->assertSet('users', function ($users) {
+            expect($users)
+                ->toHaveCount(1)
+                ->first()->name->toBe('Joe Doe');
 
             return true;
         });
