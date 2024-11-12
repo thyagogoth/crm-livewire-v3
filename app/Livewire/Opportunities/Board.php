133b26ab4382b286ad0a252bd74a3bc6d715561b
Thiago F. da Rosa <thyagogoth@gmail.com>
2024-11-12 15:35:52 -0300

    crm-25: sorting by status

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire\Opportunities;
 
+use App\Models\Opportunity;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
+/**
+ * @property-read Collection|Opportunity[] $opportunities
+ */
 class Board extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.opportunities.board');
+    }
+
+    #[Computed]
+    public function opportunities(): Collection
+    {
+        return Opportunity::query()
+            ->orderByRaw("
+                case
+                    when status = 'open' then 1
+                    when status = 'won' then 2
+                    when status = 'lost' then 3
+                end
+            ")
+            ->get();
     }
 }
--- resources/views/livewire/opportunities/board.blade.php
+++ resources/views/livewire/opportunities/board.blade.php
@@ -1,34 +1,19 @@
 <div class="p-2 grid grid-cols-3 gap-4 h-full">
 
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header title="Open" subtitle="Total: 2 opportunities" size="text-lg" class="px-2" separator progress-indicator />
+    @foreach($this->opportunities->groupBy('status') as $status => $items)
+        <div class="bg-base-200 p-2 rounded-md">
+            <x-header :title="$status"
+                      subtitle="Total: {{ $items->count() }} opportunities"
+                      size="text-lg"
+                      class="px-2"
+                      separator progress-indicator/>
 
-        <div class="space-y-2 py-2 pt-2 pb-1">
-            <x-card>Opp 1</x-card>
-            <x-card>Opp 2</x-card>
-            <x-card>Opp 4</x-card>
+            <div class="space-y-2 py-2 pt-2 pb-1">
+                @foreach($items as $item)
+                    <x-card>{{ $item->title }}</x-card>
+                @endforeach
+            </div>
         </div>
-    </div>
-
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header title="Won" subtitle="Total: 2 opportunities" size="text-lg" class="px-2" separator progress-indicator />
-
-        <div class="space-y-2 py-2 pt-2 pb-1">
-            <x-card>Opp 3</x-card>
-            <x-card>Opp 8</x-card>
-            <x-card>Opp 9</x-card>
-        </div>
-    </div>
-
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header title="Lost" subtitle="Total: 2 opportunities" size="text-lg" class="px-2" separator progress-indicator />
-
-        <div class="space-y-2 py-2 pt-2 pb-1">
-            <x-card>Opp 5</x-card>
-            <x-card>Opp 6</x-card>
-            <x-card>Opp 7</x-card>
-        </div>
-    </div>
-
+    @endforeach
 
 </div>
